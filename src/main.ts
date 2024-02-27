ec639ab2f9255342d4f7abca04ed82787f5a5caf
John Chabot <johnchabot2013@gmail.com>
2024-02-27 16:12:59 -0500

    Drop some unneeded stuff

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { wait } from './wait'
 
 /**
  * The main function for the action.
@@ -8,18 +7,16 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const example: string = core.getInput('example')
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`The example input ${example}`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`)
+    core.debug(`The event payload: ${payload}`);
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
